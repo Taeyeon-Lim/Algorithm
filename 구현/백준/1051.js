12de3d8352ff5195826ed9ceca7e196bc6a20cd4
Teayeon-Lim <wingofyeon@naver.com>
2024-04-21 16:57:02 +0900

    로컬 Node 실행 환경 구성

---
+++ 구현/백준/1051.js
@@ -0,0 +1,38 @@
+function solution(input) {
+  const [size, ...rect] = input;
+  const [N, M] = size.split(' ').map(Number);
+  let result = 1;
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      const number = rect[i][j];
+
+      for (let k = j + 1; k < M; k++) {
+        if (number !== rect[i][k]) continue;
+        const currentRow = i + k - j;
+
+        if (currentRow >= N) continue;
+        if (number !== rect[currentRow][j]) continue;
+        if (number !== rect[currentRow][k]) continue;
+
+        result = Math.max(result, k - j + 1);
+      }
+    }
+  }
+
+  console.log(result ** 2);
+}
+
+// 테스트 코드
+const testCase = [
+  [`3 5\n42101\n22100\n22101`, 9],
+  [`2 2\n12\n34`, 1],
+  [`2 4\n1255\n3455`, 4],
+  [`1 10\n1234567890`, 1],
+  [
+    `11 10\n9785409507\n2055103694\n0861396761\n3073207669\n1233049493\n2300248968\n9769239548\n7984130001\n1670020095\n8894239889\n4053971072`,
+    49,
+  ],
+];
+
+testCase.forEach(input => solution(input[0].toString().trim().split('\n')));
